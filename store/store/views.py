4e77c1ef0833db1f48dca16c4a0c45d9758eaf66
Cristian Sifuentes <sifuentescovarrubiasitsl@gmail.com>
2020-09-07 17:34:40 -0500

    20200907: Diferents points like send information to view using the context and learn about conditions and operations

--- store/store/views.py
+++ store/store/views.py
@@ -1,4 +1,25 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def index(request):
-    return HttpResponse('Hello world!')
+    return render(request,'index.html', {
+        'message' : 'Product list', 
+        'title': 'Products',
+        'products': [
+            {
+                'title': 'T-shirt', 
+                'prince':5, 
+                'stock': True
+            },
+            {
+                'title': 'Bag', 
+                'prince':2, 
+                'stock': True
+            },
+            {
+                'title': 'Pants', 
+                'prince':10, 
+                'stock': False
+            }         
+        ]
+    })
