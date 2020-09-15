ce4e284575ebcf6bd0c60d1cddee591093417907
Cristian Sifuentes <sifuentescovarrubiasitsl@gmail.com>
2020-09-15 18:26:21 -0500

    20200915: Implementing DetailView for to show information about a specific product

--- store/products/views.py
+++ store/products/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 from products.models import Product
 
 # Create your views here.
@@ -11,5 +12,14 @@ class ProductListView(ListView):
         context = super().get_context_data(**kwargs)
         context['message'] = 'Product list'
         context['products'] = context['product_list']
+        print(context)
+        return context
+
+class ProductDetailView(DetailView):#id -> pk
+    model = Product
+    template_name = 'products/product.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
         print(context)
         return context
--- store/store/urls.py
+++ store/store/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from . import views
 from products.views import ProductListView
 
@@ -8,6 +8,7 @@ urlpatterns = [
     path('', ProductListView.as_view(), name='index'),
     path('user/login', views.login_view, name='login_view'),
     path('user/logout', views.logout_view,name='logout_view'),
-    path('user/register', views.register_view,name='register_view')
+    path('user/register', views.register_view,name='register_view'),
+    path('products/', include('products.urls'))
 
 ]
