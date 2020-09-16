e15e8013de57eed6a15a5aa4ca9b4014c214b908
Cristian Sifuentes <sifuentescovarrubiasitsl@gmail.com>
2020-09-16 13:07:34 -0500

    20200916: We created a slug field for search a using this field and we also modified admin.py for get a better way for show fields in admin panel

--- store/products/admin.py
+++ store/products/admin.py
@@ -3,4 +3,11 @@ from django.contrib import admin
 # Register your models here.
 from .models import Product
 
-admin.site.register(Product)
+# Se modifica que campos se puede ver en el administrador
+class ProductAdmin(admin.ModelAdmin):
+    fields = ('title', 'description', 'price')
+    list_display = ('__str__', 'slug', 'created_at')
+
+# admin.site.register(Product)
+admin.site.register(Product, ProductAdmin)
+
