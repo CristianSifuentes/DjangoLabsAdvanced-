1cc9e19ceabde59178d3e7900dbdd2ebba0aed54
Cristian Sifuentes <sifuentescovarrubiasitsl@gmail.com>
2020-09-16 13:34:10 -0500

    20200916: Other way for create a slug using django.db.models.signals.pre_save

--- store/products/models.py
+++ store/products/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils.text import slugify
+from django.db.models.signals import pre_save
 
 # Create your models here.
 class Product(models.Model):
@@ -9,9 +10,15 @@ class Product(models.Model):
     slug = models.SlugField(null=False, blank=False,unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
     
-    def save(self, *args, **kwargs):
-        self.slug = slugify(self.title)
-        super(Product, self).save(*args, **kwargs)
+    #sobre escribe el metodo save y se auto-crea un slug
+    # def save(self, *args, **kwargs):
+    #     self.slug = slugify(self.title)
+    #     super(Product, self).save(*args, **kwargs)
     
     def __str__(self):
         return self.title
+
+def set_slug(sender, instance, *args, **kwargs):#callback
+    instance.slug = slugify(instance.title)
+
+pre_save.connect(set_slug, sender=Product)
