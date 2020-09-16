da112d20de2bb87f270e2e9fa6a3f4693fb9e2fa
Cristian Sifuentes <sifuentescovarrubiasitsl@gmail.com>
2020-09-16 14:12:11 -0500

    20200916: Implementing unique slugs using uuid

--- store/products/models.py
+++ store/products/models.py
@@ -1,3 +1,4 @@
+import uuid
 from django.db import models
 from django.utils.text import slugify
 from django.db.models.signals import pre_save
@@ -19,6 +20,15 @@ class Product(models.Model):
         return self.title
 
 def set_slug(sender, instance, *args, **kwargs):#callback
-    instance.slug = slugify(instance.title)
+    if instance.title and not instance.slug:
+        slug = slugify(instance.title)
+        
+        while Product.objects.filter(slug=slug).exists():
+            slug = slugify(
+                '{}-{}'.format(instance.title, str(uuid.uuid4()) )
+            )
+
+        instance.slug = slug
+     #instance.slug = slugify(instance.title)
 
 pre_save.connect(set_slug, sender=Product)
