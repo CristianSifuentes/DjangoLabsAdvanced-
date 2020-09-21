b02a77798af0879f6e4b30d279e440fbb2b111ea
Cristian Sifuentes <sifuentescovarrubiasitsl@gmail.com>
2020-09-21 16:02:09 -0500

    20200921: Creating specific urls por products

--- store/products/urls.py
+++ store/products/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = 'products'
+
 urlpatterns = [
     path('search', views.ProductSearchListView.as_view(), name="productsearch_view"),
     path('<slug:slug>', views.ProductDetailView.as_view(), name='product_view' )
