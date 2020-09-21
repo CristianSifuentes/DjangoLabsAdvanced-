0409de16a1aaed5521f6bf41a5d3cf89fb93f34a
Cristian Sifuentes <sifuentescovarrubiasitsl@gmail.com>
2020-09-21 13:49:35 -0500

    20200921: We have created a new modulo for categories

--- store/store/settings.py
+++ store/store/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'products'
+    'products',
+    'categories'
 ]
 
 MIDDLEWARE = [
